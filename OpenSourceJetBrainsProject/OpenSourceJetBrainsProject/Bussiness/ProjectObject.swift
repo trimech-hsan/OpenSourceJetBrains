fd3c5960903f58adfdfaa52156fca4aeb002f22c
Trimeche Hassen <trimech.hsan@gmail.com>
2022-11-26 17:37:36 +0100

    create ProjectObject
    Decodable  and CodingKey

---
+++ OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/Bussiness/ProjectObject.swift
@@ -0,0 +1,28 @@
+//
+//  ProjectObject.swift
+//  OpenSourceJetBrainsProject
+//
+//  Created by Trimeche Hassen on 26/11/2022.
+//
+
+import Foundation
+
+struct ProjectObject: Decodable {
+    
+    var fullName : String?
+    var forks : Int?
+    var openIssues : Int?
+    var watchers : Int?
+    var description : String?
+
+
+    enum CodingKeys: String, CodingKey {
+        
+        case fullName = "full_name"
+        case forks = "forks"
+        case openIssues = "open_issues"
+        case watchers = "watchers"
+        case description = "description"
+
+    }
+}
