bcffeaf3a270ef9e1b75212af6507d25b10d3770
Trimeche Hassen <trimech.hsan@gmail.com>
2022-11-26 16:12:54 +0100

    Create XcodeProject

---
+++ OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenSourceJetBrainsProject
+//
+//  Created by Trimeche Hassen on 26/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
