2a5b2a35fa56c9acae1a2433ce8abfe7908a7d0a
Trimeche Hassen <trimech.hsan@gmail.com>
2022-11-26 17:42:13 +0100

    Prepare ProjectsModel and ProjectsViewModel
    add functions fetchProjects

--- OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/Screens/ProjectsPage/ProjectsModel.swift
+++ OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/Screens/ProjectsPage/ProjectsModel.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 class ProjectsModel {
+    var projectsService: projectsServiceInterface
+
+    init(projectsService: projectsServiceInterface = ProjectsService()){
+        self.projectsService = projectsService
+    }
     
-    
+    func fetchProjects(page: Int) async throws -> [ProjectObject]? {
+        do {
+            // await : attendre la reponse de la fonction asynchrone
+            //  c'est le point de suspension
+           return try await projectsService.fetchProjects(page: page)
+        } catch {
+            return nil
+        }
+    }
 }
--- OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/Screens/ProjectsPage/ProjectsViewModel.swift
+++ OpenSourceJetBrainsProject/OpenSourceJetBrainsProject/Screens/ProjectsPage/ProjectsViewModel.swift
@@ -10,7 +10,11 @@ import Foundation
 
 class ProjectsViewModel {
     var model: ProjectsModel
-  
+    var allProjects: [ProjectObject] = []
+    var page: Int = 1
+    var lastCount: Int = 0
+    var allPagesAreLoaded: Bool = false
+    
     init(model: ProjectsModel) {
         self.model = model
     }
@@ -19,9 +23,41 @@ class ProjectsViewModel {
         return "Open-source projects"
     }
     func getNumberOfRows() -> Int {
-        return 10
+        return allProjects.count()
     }
     
-   
+    func fetchProjects() async throws {
+        
+        do {
+            lastCount = self.allRepos.count
+            let result = try await model.fetchProjects(page: page)
+            if let sortedResult  = result{
+                self.allProjects.append(contentsOf: sortedResult)
+            }
+        } catch {
+            print("Request failed with error: \(error)")
+            self.feedingError(error)
+        }
+    }
+    // récuperer le Projects selectioné
+    func getProjects(atIndex: Int) -> Repo? {
+        
+        if atIndex < allProjects.count {
+            return allProjects[atIndex]
+        }
+        return nil
+    }
+    
+    func shouldLoadNextPage(index: Int) -> Bool {
+        if allProjects.isEmpty == false, index == (allProjects.count - 1 ) {
+            if self.lastCount == allProjects.count {
+                allPagesAreLoaded = true
+                return false
+            }
+            page += 1
+            return true
+        }
+        return false
+    }
 }
 
